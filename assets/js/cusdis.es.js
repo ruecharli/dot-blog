fd6d2f5ab04c7082d00c52ad0d9827d0e7fb672b
Rue Charli <ruerieschiek@gmail.com>
2025-11-24 15:49:43 +0800

    Update cusdis.es.js

--- assets/js/cusdis.es.js
+++ assets/js/cusdis.es.js
@@ -2,7 +2,7 @@ window.CUSDIS = {};
 const makeIframeContent = (target) => {
   const host = target.dataset.host || "https://cusdis.com";
   const iframeJsPath = target.dataset.iframe || `${host}/js/iframe.umd.js`;
-  const cssPath = `https://ruecharli.blog/assets/style.css`;
+  const cssPath = `https://ruecharli.blog/assets/main.css`;
   return `<!DOCTYPE html>
 <html>
   <head>
